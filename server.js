5045fb0f20d0a98cf2d9e33c855adbfd3c00037e
Diana García <diana.eli.gg@gmail.com>
2020-07-14 18:38:36 -0500

    Added some api key

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const connectionInfo = {
   server: "localhost",
   user: "root",
-  password: process.env.MYSQL_PASSWORD
-} 
+  password: process.env.MYSQL_PASSWORD,
+};
 
+const someApiKey = process.env.SOME_API_KEY;
 
 // mysql.connect(connectionInfo);
 
 console.log("=".repeat(40));
 console.log(connectionInfo);
+console.log(someApiKey);
 console.log("=".repeat(40));
